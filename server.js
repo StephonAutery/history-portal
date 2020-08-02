aeb9c04824afee40ba707554a1ae142ec4d87b67
StephonAutery <stephon@stephonautery.com>
2020-08-02 10:05:21 -0700

    qgtunnel

--- server.js
+++ server.js
@@ -11,29 +11,29 @@ const routes = require("./routes");
 
 
 // ################ QuotaGuard #####################
-// var http, options, proxy, url;
+var http, options, proxy, url;
 
-// http = require("http");
+http = require("http");
 
-// url = require("url");
+url = require("url");
 
-// proxy = url.parse("process.env.QUOTAGUARDSTATIC_URL");
-// target  = url.parse("https://history-portal.herokuapp.com");
+proxy = url.parse("process.env.QUOTAGUARDSTATIC_URL");
+target  = url.parse("https://history-portal.herokuapp.com");
 
-// options = {
-//   hostname: proxy.hostname,
-//   port: proxy.port || 80,
-//   path: target.href,
-//   headers: {
-//     "Proxy-Authorization": "Basic " + (new Buffer.from("proxy.auth").toString("base64")),
-//     "Host" : target.hostname
-//   }
-// };
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer.from("proxy.auth").toString("base64")),
+    "Host" : target.hostname
+  }
+};
 
-// http.get(options, function(res) {
-//   res.pipe(process.stdout);
-//   return console.log("status code", res.statusCode);
-// });
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
 // ################ QuotaGuard #####################
 
 const app = express();
